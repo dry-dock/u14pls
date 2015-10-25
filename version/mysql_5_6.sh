0a95f91e14bd0203dbe63a48b03c4db301ea72e4
Avi Cavale <avi@shippable.com>
2015-10-25 15:00:49 -0700

    updating mysql

--- version/mysql_5_6.sh
+++ version/mysql_5_6.sh
@@ -1,26 +1,29 @@
 #!/bin/bash -e
 
 echo "================= setting MySQL preReqs ==================="
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password aaa'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password aaa'
+sudo groupadd mysql
+sudo useradd -g mysql mysql
+apt-get install libaio1
+mkdir -p /tmp
+chmod 1777 /tmp
+mkdir -p /etc/mysql
 
 echo "=========== Downloading mysql 5.6 ==============="
-cd /tmp && wget http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.23-debian6.0-x86_64.deb -O mysql-5.6.23-debian6.0-x86_64.deb;
-
-echo "=========== Installing mysql 5.6 ==============="
-dpkg -i  mysql-5.6.23-debian6.0-x86_64.deb;
-rm mysql-5.6.23-debian6.0-x86_64.deb
+cd /usr/local
+wget -O mysql-5.6.26-linux-glibc2.5-x86_64.tar.gz https://downloads.mariadb.com/archives/mysql-5.6/mysql-5.6.26-linux-glibc2.5-x86_64.tar.gz
+tar xvfz mysql-5.6.26-linux-glibc2.5-x86_64.tar.gz
+mv mysql-5.6.26-linux-glibc2.5-x86_64 mysql
+rm mysql-5.6.26-linux-glibc2.5-x86_64.tar.gz
+cd mysql
+chown -R mysql .
+chgrp -R mysql .
 
 echo "================= Configuring MySQL ==================="
-chown -R mysql /opt/mysql/server-5.6/
-chgrp -R mysql /opt/mysql/server-5.6/
+scripts/mysql_install_db --user=mysql
+chown -R root .
+chown -R mysql data
+cp support-files/mysql.server /etc/init.d/mysql.server
+cp -rf my.cnf /usr/local/mysql/my.cnf
 
-ln -sf /opt/mysql/server-5.6/bin/mysqld_safe /usr/bin/mysqld_safe
-
-sudo /usr/bin/mysqld_safe &
-sleep 5
-mysqladmin -u root -p'aaa' password ''
-echo "GRANT ALL ON *.* TO shippable@localhost IDENTIFIED BY ''; FLUSH PRIVILEGES;" | mysql -uroot
-echo "GRANT ALL ON *.* TO travis@localhost IDENTIFIED BY ''; FLUSH PRIVILEGES;" | mysql -uroot
-echo "GRANT ALL ON *.* TO travis IDENTIFIED BY ''; FLUSH PRIVILEGES;" | mysql -uroot
-echo "GRANT ALL ON *.* TO ''@localhost IDENTIFIED BY ''; FLUSH PRIVILEGES;" | mysql -uroot
+echo "=========== Installing mysql clients 5.6 ==============="
+apt-get install mysql-client-5.6
