c910a0fbe7038c83f2b8fe7fcdb345aebc845924
Ragesh Krishna <ragesh@rageshkrishna.com>
2015-12-11 11:51:24 +0530

    Revert "Add postgres 9.1, 9.2, 9.3 and postgis for all versions"

--- install.sh
+++ install.sh
@@ -9,17 +9,10 @@ do
 done
 
 echo "================= Adding mysql cnf ==================="
-cd /u14pls/config/mysql && cp -rf my.cnf /etc/mysql/my.cnf
+cd /u14pls && cp -rf my.cnf /etc/mysql/my.cnf
 
 echo "================= Adding PostgreSQL cnf ==================="
-cd /u14pls/config/postgres && cp -rf pg_hba-9.1.conf /etc/postgresql/9.1/main/pg_hba.conf
-cd /u14pls/config/postgres && cp -rf pg_hba-9.2.conf /etc/postgresql/9.2/main/pg_hba.conf
-cd /u14pls/config/postgres && cp -rf pg_hba-9.3.conf /etc/postgresql/9.3/main/pg_hba.conf
-cd /u14pls/config/postgres && cp -rf pg_hba-9.4.conf /etc/postgresql/9.4/main/pg_hba.conf
-cd /u14pls/config/postgres && cp -rf postgresql-9.1.conf /etc/postgresql/9.1/main/postgresql.conf
-cd /u14pls/config/postgres && cp -rf postgresql-9.2.conf /etc/postgresql/9.2/main/postgresql.conf
-cd /u14pls/config/postgres && cp -rf postgresql-9.3.conf /etc/postgresql/9.3/main/postgresql.conf
-cd /u14pls/config/postgres && cp -rf postgresql-9.4.conf /etc/postgresql/9.4/main/postgresql.conf
+cd /u14pls && cp -rf pg_hba.cnf /etc/postgresql/9.4/main/pg_hba.conf
 
 echo "================= Cleaning package lists ==================="
 apt-get clean
