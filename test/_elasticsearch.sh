1cdb2ae97fd9e0b4f12ef2486610cbdc06dede74
harsh157 <harsh12213@gmail.com>
2016-02-08 20:56:16 +0530

    add test for all services

--- test/_elasticsearch.sh
+++ test/_elasticsearch.sh
@@ -14,61 +14,10 @@ start_service() {
 
 # Function to stop
 stop_service() {
-  curl -s -X POST "http://localhost:9200/_shutdown" > /dev/null 2>&1;
+  curl -s -X POST "http://localhost:9200/_shutdown" 2>&1;
 }
 
-start_generic_service() {
-  name=$1
-  binary=$2
-  service_cmd=$3
-  service_port=$4
-
-
-  if [ -f $binary ]; then
-    exec_cmd "sudo su -c \"$service_cmd > /dev/null 2>&1 &\"";
-    sleep 5
-
-    ## check if the service port is reachable
-    while ! nc -vz localhost $service_port &>/dev/null; do
-
-      ## check service process PID
-      service_proc=$(pgrep -f "$binary" || echo "")
-
-      if [ ! -z "$service_proc" ]; then
-        ## service PID exists, service is starting. Hence wait...
-        exec_cmd "echo \"Waiting for $name to start...\"";
-      else
-        ## service PID does not exist, service crashed. Reboot service...
-        exec_cmd "echo 'Service $name boot error, restarting...'"
-        exec_cmd "sudo su -c \"$service_cmd > /dev/null 2>&1 &\"";
-      fi
-      sleep 5;
-    done
-    exec_cmd "echo \"$name started successfully\"";
-  else
-    exec_cmd "echo \"$name will not be started because the binary was not found at $binary.\""
-    if [ "$name" != "mysql" ] && [ "$name" != "postgres" ]; then
-      exit 99;
-    fi
-  fi
-}
-
-exec_cmd() {
-  cmd=$@
-  cmd_uuid=$(python -c 'import uuid; print(str(uuid.uuid4()))')
-  cmd_start_timestamp=`date +"%s"`
-  echo "__SH__CMD__START__|{\"type\":\"cmd\",\"sequenceNumber\":\"$cmd_start_timestamp\",\"id\":\"$cmd_uuid\"}|$cmd"
-  eval "$cmd"
-  cmd_status=$?
-  if [ "$2" ]; then
-    echo $2;
-  fi
-
-  cmd_end_timestamp=`date +"%s"`
-  echo "__SH__CMD__END__|{\"type\":\"cmd\",\"sequenceNumber\":\"$cmd_start_timestamp\",\"id\":\"$cmd_uuid\",\"exitcode\":\"$cmd_status\"}|$cmd"
-  return $cmd_status
-}
-
+source /u14pls/test/function_start_generic.sh
 #
 # Call to start service
 #
---
+++ test/_mysql.sh
@@ -0,0 +1,33 @@
+# Begin service ENV variables
+export SHIPPABLE_START_MYSQL=true;
+export SHIPPABLE_MYSQL_PORT=3306;
+export SHIPPABLE_MYSQL_BINARY="/usr/bin/mysqld_safe";
+export SHIPPABLE_MYSQL_CMD="$SHIPPABLE_MYSQL_BINARY";
+# End service ENV variables
+
+#
+# Function to START
+#
+start_service() {
+  start_generic_service "mysql" "$SHIPPABLE_MYSQL_BINARY" "$SHIPPABLE_MYSQL_CMD" "$SHIPPABLE_MYSQL_PORT";
+}
+
+#
+# Function to STOP
+#
+stop_service() {
+  exec_cmd "mysqladmin shutdown"
+}
+
+source /u14pls/test/function_start_generic.sh
+#
+# Call to start service
+#
+echo "================= Starting mysql ==================="
+printf "\n"
+start_service
+printf "\n\n"
+echo "================= Stopping mysql ==================="
+printf "\n"
+stop_service
+printf "\n\n"
